49b412d78d8d29365bb3ead1cf093c58eaea7390
DakhariE <80596335+DakhariE@users.noreply.github.com>
2022-07-08 23:44:22 -0400

    Created class for projects and implemented data base to render projects. - Need to figure out how to render images using ImageField()

--- base/website/models.py
+++ base/website/models.py
@@ -1,3 +1,16 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=10000)
+    photo = models.ImageField()
+    description = models.CharField(max_length=1000000)
+    tech = models.CharField(max_length=10000)
+    gitlink =  models.URLField()
+    codelink = models.URLField()
+
+    def __str__(self):
+      return self.name
+
--- base/website/views.py
+++ base/website/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  projects = Project.objects.all()
+  return render(request, 'index.html', {'projects': projects})
+
+def projects(request, pk):
+  projects = Project.objects.get(id=pk)
+  return render(request, 'index.html', {'projects': projects})
